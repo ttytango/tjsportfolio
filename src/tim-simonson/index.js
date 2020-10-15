a8145a6d07fcfe80f45f8fffecad5d439c7f353f
ttytango <59534146+ttytango@users.noreply.github.com>
2020-10-15 17:16:58 +0100

    more styling completed

--- src/tim-simonson/index.js
+++ src/tim-simonson/index.js
@@ -1,11 +1,11 @@
-window.onscroll = function() { scrollFunction() };
-var header = document.getElementById("header");
+// window.onscroll = function() { scrollFunction(); };
+// var header = document.getElementById("header");
 
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        header.style.height = "240px";
-    } else {
-        header.style.height = "200px";
-        header.style.paddingTop = "20px";
-    }
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//         header.style.height = "440px";
+//     } else {
+//         header.style.height = "220px";
+//         // header.style.paddingTop = "20px";
+//     }
+// }
